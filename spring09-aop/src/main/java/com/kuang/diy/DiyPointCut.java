de2eb5d971d14339daf74367dc437c094494d582
heiye5 <2716634948@qq.com>
2021-05-15 19:55:03 +0800

    Spring-AOP实现方式二

---
+++ spring09-aop/src/main/java/com/kuang/diy/DiyPointCut.java
@@ -0,0 +1,11 @@
+package com.kuang.diy;
+
+public class DiyPointCut {
+    public void before(){
+        System.out.println("前");
+    }
+
+    public void after(){
+        System.out.println("后");
+    }
+}
